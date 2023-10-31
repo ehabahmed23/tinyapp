bb9e063535f2ef1c7754d53020d96b712bc4b9c8
Ehab Ahmed <ehabahmed2456@gmail.com>
2023-10-31 15:04:23 -0400

    added routes for a post request

--- express_server.js
+++ express_server.js
@@ -3,6 +3,10 @@ const app = express();
 const PORT = 8080;
 
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+
+function generateRandomString() {}
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -30,6 +34,10 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 /*
 this get format /urls/:id. The : in front of id indicates that id is a route parameter. 
 This means that the value in this part of the url will be available in the req.params object.
@@ -37,4 +45,9 @@ This means that the value in this part of the url will be available in the req.p
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]};
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
